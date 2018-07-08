d1841e4048276d0eb12b50701c34833caca57aea
Anna Selivanova <foxy.eyed@gmail.com>
2018-07-08 21:58:10 +0400

    Add User module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello react!')
+import User from './src/User';
+
+const customer = new User('John', 'Doe', '1990-12-12');
+
+console.log(customer.fullName());
+console.log(customer.age());
