413b6dd2742a00679aa12a5ba02eee43a099cac3
Anna Selivanova <foxy.eyed@gmail.com>
2018-07-15 22:12:12 +0400

    Add invoice creation feature (#2)

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import User from './src/User';
+import Invoice from './src/Invoice';
 import Hello from './src/Hello';
 
 const customer = new User('John', 'Doe', '1990-12-12');
+const goods = [
+  { name: 'apple', price: 5 },
+  { name: 'orange', price: 4 },
+  { name: 'tomato', price: 3 },
+];
 
-ReactDOM.render(<Hello userName={customer.fullName()} />, document.querySelector('#app'));
+ReactDOM.render(
+  <div>
+    <Hello userName={customer.fullName()} />
+    <Invoice goods={goods} />
+  </div>,
+  document.querySelector('#app'),
+);
