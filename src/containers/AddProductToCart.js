4c54ab5486fc4e4b959f07343fac55f7902b3312
Anna Selivanova <foxy.eyed@gmail.com>
2018-08-03 18:09:50 +0400

    Add input to specify quantity of product items adding to cart

---
+++ src/containers/AddProductToCart.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, Input, Button } from 'reactstrap';
+
+import { productPropType } from '../constants/propTypes';
+import { CartContextConsumer } from './CartProvider';
+
+class AddProductToCart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: props.product,
+      quantity: 1,
+    };
+
+    this.handleChangeQuantity = this.handleChangeQuantity.bind(this);
+  }
+
+  handleChangeQuantity(e) {
+    const { target } = e;
+    this.setState({
+      quantity: parseInt(target.value, 10) || 1,
+    });
+  }
+
+  render() {
+    const { product, quantity } = this.state;
+
+    return (
+      <CartContextConsumer>
+        {({ addToCart }) => (
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addToCart(product, quantity);
+            }}
+          >
+            <div className="form-row">
+              <FormGroup className="col-md-6">
+                <Input
+                  type="number"
+                  name="quantity"
+                  value={quantity}
+                  onChange={this.handleChangeQuantity}
+                  required
+                />
+              </FormGroup>
+              <FormGroup className="col-md-6">
+                <Button color="success">
+                  Add
+                </Button>
+              </FormGroup>
+            </div>
+          </Form>
+        )}
+      </CartContextConsumer>
+    );
+  }
+}
+
+AddProductToCart.propTypes = {
+  product: productPropType.isRequired,
+};
+
+export default AddProductToCart;
